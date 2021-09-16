d8a70c06cf6a268d274fd4c6ba65e5b3b356ef2b
AxelNordov <iq@ua.fm>
2021-09-16 21:56:50 +0300

    put 'uses' back in place
    BUILD was FAILED without 'uses'

--- src/main/java/com/example/userservicetest/mapper/UserMapper.java
+++ src/main/java/com/example/userservicetest/mapper/UserMapper.java
@@ -4,8 +4,8 @@ import com.example.userservicetest.entity.User;
 import org.mapstruct.*;
 import package_com.example.userservicetest.model.UserDto;
 
-@Mapper(componentModel = "spring")
-//@Mapper(componentModel = "spring", uses = {UserGroupMapper.class})
+//@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {UserGroupMapper.class})
 public interface UserMapper {
 
     UserDto userToUserDto(User user);
